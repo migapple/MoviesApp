3659aded0cf9f1d7483c01ebda465634d99458bc
Michel Garlandat <michel.garlandat@me.com>
2021-09-04 12:24:52 +0200

    Ajout movielistscreen

---
+++ MoviesApp/Screens/MovieListScreen.swift
@@ -0,0 +1,30 @@
+//
+//  MovieListScreen.swift
+//  MovieListScreen
+//
+//  Created by Michel Garlandat on 04/09/2021.
+//  Copyright © 2021 Mohammad Azam. All rights reserved.
+//
+
+import SwiftUI
+
+struct MovieListScreen: View {
+    @ObservedObject private var movieListVM: MovieListViewModel
+    
+    init() {
+        self.movieListVM = MovieListViewModel()
+        self.movieListVM.searchByName("batman")
+    }
+    
+    var body: some View {
+        VStack {
+            MovieListView(movies: self.movieListVM.movies)
+        }
+    }
+}
+
+struct MovieListScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        MovieListScreen()
+    }
+}
